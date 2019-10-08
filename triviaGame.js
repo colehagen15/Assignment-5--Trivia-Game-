e5b725f42986c75abe4505d58b8ea38f9a2f70c7
colehagen15 <54552528+colehagen15@users.noreply.github.com>
2019-10-08 16:16:03 -0500

    Began creation of game

--- triviaGame.js
+++ triviaGame.js
@@ -1,8 +1,33 @@
+let questionLbl = $(`#questionLbl`);
+let questionNumber = $(`#questionNumber`);
+let guessBtn = $(`#check`);
+let feedBack = $(`#feedBack`);
+let nextQBtn = $(`#nextQuestion`);
+let userGuess = $(`#userGuess`);
+
+
 const apiUrl = `https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean`;
     $.ajax({
         url: apiUrl
     }).then((data)=> {
       console.log(JSON.stringify(data,null,3))
-      console.log(data.results[0].question)
+    
+    var i = -1;
+    nextQBtn.click(function() {
+        i += 1;
+        console.log(data.results[i].question)
+        questionLbl.text(`${data.results[i].question}`);
         
-});
+    guessBtn.click(function(){
+        console.log(data.results[i].correct_answer)
+        if (userGuess.val() == (data.results[i].correct_answer)){
+            feedBack.text(`Correct!`);
+        }
+
+        });
+    });
+
+
+
+
+    });
