99f252ea0aae63e269f3c05684b961d4ec50ab98
colehagen15 <54552528+colehagen15@users.noreply.github.com>
2019-10-08 20:44:42 -0500

    Made the game function

--- triviaGame.js
+++ triviaGame.js
@@ -4,29 +4,56 @@ let guessBtn = $(`#check`);
 let feedBack = $(`#feedBack`);
 let nextQBtn = $(`#nextQuestion`);
 let userGuess = $(`#userGuess`);
+let count = $(`#count`);
 
+guessBtn.attr("disabled", true);
 
+var correctAnswers = 0;
+count.text(`Score: ` + correctAnswers)
+nextQBtn.text("Start");
 const apiUrl = `https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean`;
     $.ajax({
         url: apiUrl
     }).then((data)=> {
       console.log(JSON.stringify(data,null,3))
     
-    var i = -1;
+    var i = 0;
     nextQBtn.click(function() {
-        i += 1;
+        guessBtn.attr("disabled", false);
+        nextQBtn.text("Next Question");
+        nextQBtn.hide();
+        feedBack.hide();
         console.log(data.results[i].question)
         questionLbl.text(`${data.results[i].question}`);
+        userGuess.val('');
         
-    guessBtn.click(function(){
+      });   
+    
+      guessBtn.click(function() {
+        nextQBtn.show();
+        feedBack.show();
+        console.log(i);
         console.log(data.results[i].correct_answer)
         if (userGuess.val() == (data.results[i].correct_answer)){
             feedBack.text(`Correct!`);
+            correctAnswers += 1;
+            ++i;
         }
-
-        });
+        else if (userGuess.val() != (data.results[i].correct_answer) && userGuess.val() == "True" || userGuess.val() == "False"){
+            feedBack.text(`Wrong.`);
+            ++i;
+        }
+        else if (userGuess.val() != 'True' || userGuess.val() != 'False'){
+            feedBack.text("Guess must be True or False (Capitalization is important)");
+        }
+        count.text("Score: "+ correctAnswers);
+        if (i == 10) {
+            feedBack.text("Total Score: "+ correctAnswers + "/" + "10");
+            nextQBtn.attr("disabled", true);
+            guessBtn.attr("disabled", true);
+        }
     });
-
+    
 
 
 
